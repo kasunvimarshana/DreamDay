f61b3de7425a710159f9f354acb9255fc4d14036
Kasun Vimarshana <kasunvmail@gmail.com>
2025-05-25 17:42:22 +0530

    Create 'EmailService'

---
+++ DreamDay.BLL/Services/Implementations/EmailService.cs
@@ -0,0 +1,48 @@
+﻿using DreamDay.BLL.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamDay.BLL.Services.Implementations
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string to, string subject, string body)
+        {
+            var smtpHost = _configuration["MailSettings:Host"];
+            var smtpPort = int.Parse(_configuration["MailSettings:Port"]);
+            var smtpUser = _configuration["MailSettings:Username"];
+            var smtpPass = _configuration["MailSettings:Password"];
+            var fromAddress = _configuration["MailSettings:From"];
+
+            var message = new MailMessage
+            {
+                From = new MailAddress(fromAddress),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+            message.To.Add(to);
+
+            using var client = new SmtpClient(smtpHost, smtpPort)
+            {
+                Credentials = new NetworkCredential(smtpUser, smtpPass),
+                EnableSsl = true
+            };
+
+            await client.SendMailAsync(message);
+        }
+    }
+}
--- DreamDay.UI/Program.cs
+++ DreamDay.UI/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<DreamDayDbContext>(options => options.UseSqlServer
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IEmailService, EmailService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
