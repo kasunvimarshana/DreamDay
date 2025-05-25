dfe2fe676ca67a6247a0b06f694b613e187762ce
Kasun Vimarshana <kasunvmail@gmail.com>
2025-05-25 13:27:47 +0530

    Configure Dependency Injection (DI Container)

--- DreamDay.UI/Program.cs
+++ DreamDay.UI/Program.cs
@@ -1,4 +1,19 @@
+using DreamDay.BLL.Services.Implementations;
+using DreamDay.BLL.Services.Interfaces;
+using DreamDay.DAL.Context;
+using DreamDay.DAL.Repositories.Implementations;
+using DreamDay.DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<DreamDayDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
