bfb42b622ad5c4a29f6f6ecfe5d519a4bb2cb860
Kasun Vimarshana <kasunvmail@gmail.com>
2025-05-30 01:26:23 +0530

    Create Dashboard

---
+++ DreamDay.Models/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,21 @@
+﻿using DreamDay.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DreamDay.Models.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public int TotalWeddings { get; set; }
+        public int UpcomingWeddings { get; set; }
+        public int TotalVenues { get; set; }
+        public int TotalUsers { get; set; }
+        public List<Wedding> RecentWeddings { get; set; }
+    }
+
+}
--- DreamDay.UI/Controllers/HomeController.cs
+++ DreamDay.UI/Controllers/HomeController.cs
@@ -1,22 +1,46 @@
-using System.Diagnostics;
+using DreamDay.BLL.Services.Interfaces;
+using DreamDay.Models.ViewModels;
 using DreamDay.UI.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
 
 namespace DreamDay.UI.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IWeddingService _weddingService;
+        private readonly IVenueService _venueService;
+        private readonly IUserService _userService;
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWeddingService weddingService, IVenueService venueService, IUserService userService)
         {
             _logger = logger;
+            _weddingService = weddingService;
+            _venueService = venueService;
+            _userService = userService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var weddings = await _weddingService.GetAllWeddingsAsync();
+            var upcomingWeddings = weddings.Where(w => w.WeddingDate >= DateTime.Today).ToList();
+            var recentWeddings = weddings.OrderByDescending(w => w.CreatedAt).Take(5).ToList();
+
+            var model = new DashboardViewModel
+            {
+                TotalWeddings = weddings.Count(),
+                UpcomingWeddings = upcomingWeddings.Count,
+                TotalVenues = (await _venueService.GetAllVenuesAsync()).Count(),
+                TotalUsers = (await _userService.GetAllUsersAsync()).Count(),
+                RecentWeddings = recentWeddings
+            };
+
+            return View(model);
         }
 
         public IActionResult Privacy()
